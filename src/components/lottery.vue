bf00206201aef6917f7d48fb4b9c98002029cf40
nigulasi <golangg@gmail.com>
2018-05-16 18:40:45 +0800

    提交

--- src/components/lottery.vue
+++ src/components/lottery.vue
@@ -926,6 +926,7 @@
                 this.CurrHxOdds = '';
             },
             changeCurrBall(v,name){
+                if(name=='官方玩法') return window.top.wAlert('官方玩法暂未开发，敬请期待！');
                 this.moreA=false;
                 this.currBall = v;
                 this.currN = name;
@@ -1166,6 +1167,7 @@
         },
         watch:{
             "currBall":function(n,o){
+//                if(this.currN=='官方玩法') return window.top.wAlert('官方玩法暂未开发，敬请期待！');
                 this.mainData = this.allData[n];
                 this.jiNav = Init.getjx[this.currT][this.currN];
                 this.currBall = n;
